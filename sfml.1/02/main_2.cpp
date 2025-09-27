9e574f24185b630c3fc4a304f7689b419b7e84fc
Potapenko Max <mpotapenko.mail@gmail.com>
2025-09-27 10:49:47 +0300

    add/convex

--- sfml.1/02/main_2.cpp
+++ sfml.1/02/main_2.cpp
@@ -12,25 +12,6 @@ sf::RectangleShape createWhiteRectangle(
     return rect;
 }
 
-void createMLetter() {
-	window.draw(createWhiteRectangle({10, 100}, {50, 100}));
-	window.draw(createWhiteRectangle({10, 40}, {50, 106}, -45));
-	window.draw(createWhiteRectangle({10, 38}, {100, 101}, 45));
-	window.draw(createWhiteRectangle({10, 100}, {100, 100}));
-}
-
-void createVLetter() {
-	window.draw(createWhiteRectangle({10, 100}, {120, 100}, -14));
-	window.draw(createWhiteRectangle({10, 100}, {170, 98}, 14));
-}
-
-void createPLetter() {
-	window.draw(createWhiteRectangle({10, 90}, {190, 110}));
-	window.draw(createWhiteRectangle({40, 10}, {240, 110}, -180));
-	window.draw(createWhiteRectangle({40, 10}, {240, 160}, 180));
-	window.draw(createWhiteRectangle({10, 40}, {240, 110}));
-}
-
 int main() {
     sf::RenderWindow window(sf::VideoMode({300, 300}), "MVP");
 
@@ -43,9 +24,21 @@ int main() {
 
         window.clear();
 
-        createMLetter();
-		createVLletter();
-		createPLetter();
+        // M
+        window.draw(createWhiteRectangle({10, 100}, {50, 100}));
+        window.draw(createWhiteRectangle({10, 40}, {50, 106}, -45));
+        window.draw(createWhiteRectangle({10, 38}, {100, 101}, 45));
+        window.draw(createWhiteRectangle({10, 100}, {100, 100}));
+
+        // V
+        window.draw(createWhiteRectangle({10, 100}, {120, 100}, -14));
+        window.draw(createWhiteRectangle({10, 100}, {170, 98}, 14));
+
+        // P
+        window.draw(createWhiteRectangle({10, 90}, {190, 110}));
+        window.draw(createWhiteRectangle({40, 10}, {240, 110}, -180));
+        window.draw(createWhiteRectangle({40, 10}, {240, 160}, 180));
+        window.draw(createWhiteRectangle({10, 40}, {240, 110}));
 
         window.display();
     }
--- sfml.1/03/main.cpp
+++ sfml.1/03/main.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({500, 500}), "Convex Shapes");
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Convex Shapes");
 
     sf::ConvexShape triangle;
     triangle.setPointCount(3);
@@ -9,6 +9,15 @@ int main() {
     triangle.setPoint(1, {200, 200});
     triangle.setPoint(2, {300, 200});
     triangle.setFillColor(sf::Color::Yellow);
+
+    sf::ConvexShape trapeze;
+    trapeze.setPointCount(4);
+    trapeze.setPosition({400, 300});
+    trapeze.setPoint(0, {-90, 0});
+    trapeze.setPoint(1, {+90, 0});
+    trapeze.setPoint(2, {+120, 60});
+    trapeze.setPoint(3, {-120, 60});
+    trapeze.setFillColor(sf::Color::Green);
 
     while (window.isOpen()) {
         while (auto event = window.pollEvent()) {
@@ -19,6 +28,7 @@ int main() {
 
         window.clear();
         window.draw(triangle);
+        window.draw(trapeze);
         window.display();
     }
 
