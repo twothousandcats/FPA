9bbddf1717483d24e040197678af056ff6ff4812
Potapenko Max <mpotapenko.mail@gmail.com>
2025-10-09 17:17:44 +0300

    add/lw4_pointer_complete

--- sfml_3/03/main.cpp
+++ sfml_3/03/main.cpp
@@ -14,10 +14,18 @@ float toDegrees(const float radians) {
     return static_cast<float>(static_cast<double>(radians) * 180.0 / M_PI);
 }
 
+float normalizeAngle(float angle) {
+    while (angle > 180.f) {
+        angle -= 360.f;
+    }
+    while (angle <= -180.f) {
+        angle += 360.f;
+    }
+
+    return angle;
+}
+
 void onMouseMove(const Event::MouseMoved evt, Vector2f &mousePosition) {
-    // cout << "mouse x=" << evt.position.x
-    //         << ", y=" << evt.position.y
-    //         << endl;
     mousePosition = Vector2f(evt.position);
 }
 
@@ -41,10 +49,22 @@ void pollEvents(RenderWindow &window, Vector2f &mousePosition) {
     }
 }
 
-void update(const Vector2f &mousePosition, ConvexShape &pointer) {
+void update(
+    const Vector2f &mousePosition,
+    ConvexShape &pointer,
+    const float &dt
+) {
     const Vector2f delta = mousePosition - pointer.getPosition();
-    const float angle = atan2(delta.y, delta.x);
-    pointer.setRotation(degrees(toDegrees(angle)));
+    const float targetAngle = toDegrees(atan2(delta.y, delta.x));
+
+    const float currentAngle = pointer.getRotation().asDegrees(); // [0, 360);
+    const float currentNormalizedAngle = normalizeAngle(currentAngle); // [-180, 180);
+    const float diffAngle = normalizeAngle(targetAngle - currentNormalizedAngle);
+
+    const float maxChange = MAX_DEGREES * dt;
+    const float clampedDiff = max(-maxChange, min(diffAngle, maxChange));
+
+    pointer.setRotation(degrees(currentNormalizedAngle + clampedDiff));
 }
 
 void renderFrame(RenderWindow &window, const ConvexShape &pointer) {
@@ -66,10 +86,11 @@ int main() {
     ConvexShape pointer;
     Vector2f mousePosition;
     init(pointer);
+    Clock clock;
 
     while (window.isOpen()) {
         pollEvents(window, mousePosition);
-        update(mousePosition, pointer);
+        update(mousePosition, pointer, clock.restart().asSeconds());
         renderFrame(window, pointer);
     }
 }
