017742b1b61422c3aaa9c9b446e0c00b39845ec0
Potapenko Max <mpotapenko.mail@gmail.com>
2025-10-23 20:04:08 +0300

    hotfix/state_calculations

--- workshop_1/workshop_1_4/main.cpp
+++ workshop_1/workshop_1_4/main.cpp
@@ -9,14 +9,19 @@ using namespace std;
 
 constexpr unsigned WINDOW_WIDTH = 800;
 constexpr unsigned WINDOW_HEIGHT = 600;
-constexpr Vector2f WINDOW_CENTER = { WINDOW_WIDTH / 2.f, WINDOW_HEIGHT / 2.f };
+constexpr Vector2f WINDOW_CENTER = {WINDOW_WIDTH / 2.f, WINDOW_HEIGHT / 2.f};
+
+enum class CatState {
+    IDLE,
+    MOVING,
+};
 
 void setOrignInCenterSprite(
     Sprite &sprite,
     const Texture &texture
 ) {
     const Vector2u textureSize = texture.getSize();
-    sprite.setOrigin({ textureSize.x / 2.f, textureSize.y / 2.f });
+    sprite.setOrigin({textureSize.x / 2.f, textureSize.y / 2.f});
 }
 
 void initCat(
@@ -24,7 +29,6 @@ void initCat(
     const Texture &texture
 ) {
     cat.setPosition(WINDOW_CENTER);
-
     setOrignInCenterSprite(cat, texture);
 }
 
@@ -39,75 +43,97 @@ void initLazerPointer(
 
 void pollEvents(
     RenderWindow &window,
-    Sprite &lazerPointer
+    Sprite &lazerPointer,
+    bool &lazerPointerVisible,
+    Vector2f &targetPosition,
+    CatState &state
 ) {
     while (const auto event = window.pollEvent()) {
         if (event->is<Event::Closed>()) {
             window.close();
         }
         if (const auto *clicked = event->getIf<Event::MouseButtonPressed>()) {
-            lazerPointer.setPosition({
-                static_cast<float>(clicked->position.x), 
+            targetPosition = {
+                static_cast<float>(clicked->position.x),
                 static_cast<float>(clicked->position.y)
-            });
+            };
+            lazerPointerVisible = true;
+            state = CatState::MOVING;
+            lazerPointer.setPosition(targetPosition);
         }
     }
 }
 
 void rotateCat(
     Sprite &cat,
-    const float targetAngle
+    const Vector2f direction
 ) {
-    const float direction = cos(targetAngle) < 0.f ? -1.f : 1.f;
-    cat.setScale({ direction, 1 });
+    constexpr float TO_LEFT_DIRECTION = -1.f;
+    const float directionScale = direction.x < 0.f
+                                     ? TO_LEFT_DIRECTION
+                                     : -TO_LEFT_DIRECTION;
+    cat.setScale({directionScale, 1.f});
 }
 
 void moveCat(
     Sprite &cat,
-    const float targetAngle,
+    CatState &catState,
+    bool &lazerPointerVisible,
     const Vector2f toTarget,
+    const float &targetDistance,
     const float dt
 ) {
     constexpr float STOP_MOVE_DISTANCE = 1.f;
-    constexpr float MOVE_SPEED = 20.f;
+    constexpr float MOVE_SPEED = 100.f;
 
-    const float targetDistance = hypot(toTarget.x, toTarget.y);
     if (targetDistance <= STOP_MOVE_DISTANCE) {
+        catState = CatState::IDLE;
+        lazerPointerVisible = false;
         return;
     }
 
+    // Перемещение — использовать нормализованный вектор
     const float maxDistance = MOVE_SPEED * dt;
     const float moveDistance = min(maxDistance, targetDistance);
-    // Единичный вектор направления
-    const Vector2f direction{
-        cos(targetAngle),
-        sin(targetAngle)
-    };
+
+    // Нормализуем вектор направления
+    const Vector2f direction = toTarget / targetDistance; // единичный вектор
+
     cat.move(direction * moveDistance);
+
+    // Поворот через scale
+    rotateCat(cat, direction);
 }
 
 void updateCat(
     Sprite &cat,
-    const Sprite &lazerPointer,
+    const Vector2f &targetPosition,
+    CatState &catState,
+    bool &lazerPointerVisible,
     const float dt
 ) {
-    const Vector2f lazerPointerPosition = lazerPointer.getPosition();
-    const Vector2f catPosition = cat.getPosition();
-    const Vector2f toTarget = lazerPointerPosition - catPosition;
-    // Целевой угл в радианах [-180, 180]
-    const float targetAngle = atan2(toTarget.y, toTarget.x);
+    if (catState == CatState::IDLE) {
+        return;
+    }
 
-    rotateCat(cat, targetAngle);
-    moveCat(cat, targetAngle, toTarget, dt);
+    const Vector2f catPosition = cat.getPosition();
+    const Vector2f toTarget = targetPosition - catPosition;
+    const float targetDistance = hypot(toTarget.x, toTarget.y);
+
+    moveCat(cat, catState, lazerPointerVisible, toTarget, targetDistance, dt);
 }
 
 void render(
     RenderWindow &window,
     const Sprite &cat,
-    const Sprite &lazerPointer
+    const Sprite &lazerPointer,
+    const bool lazerPointerVisible
 ) {
     window.clear(Color::White);
-    window.draw(lazerPointer);
+    if (lazerPointerVisible) {
+        window.draw(lazerPointer);
+    }
+
     window.draw(cat);
     window.display();
 }
@@ -127,18 +153,21 @@ int main() {
         initLazerPointer(lazerPointer, lazerPointerTexture);
 
         RenderWindow window(
-            VideoMode({ WINDOW_WIDTH, WINDOW_HEIGHT }),
+            VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}),
             "Cat moves following the lazer pointer"
         );
 
         Clock clock;
+        auto catState = CatState::IDLE;
+        Vector2f targetPosition;
+        bool lazerPointerVisible = false;
+
         while (window.isOpen()) {
-            pollEvents(window, lazerPointer);
-            updateCat(cat, lazerPointer, clock.restart().asSeconds());
-            render(window, cat, lazerPointer);
+            pollEvents(window, lazerPointer, lazerPointerVisible, targetPosition, catState);
+            updateCat(cat, targetPosition, catState, lazerPointerVisible, clock.restart().asSeconds());
+            render(window, cat, lazerPointer, lazerPointerVisible);
         }
-        
-    } catch (const sf::Exception& error) {
+    } catch (const sf::Exception &error) {
         cerr << "SFML Error: " << error.what() << endl;
         return EXIT_FAILURE;
     }
